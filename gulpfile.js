efa0dbebb69ecb857c75a25f8ccb48141653733e
Luis Santos <lm.santos@campus.fct.unl.pt>
2018-12-22 19:29:22 +0000

    Add gulp Autoprefixer and SourceMap to DevDependecies

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@ var minifyCSS = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var changed = require('gulp-changed');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
 
 
 
@@ -19,37 +21,37 @@ var SCSS_SRC = './src/Assets/scss/**/*.scss';
 var SCSS_DEST = './src/Assets/css';
 
 // Compile SCSS min
-gulp.task('compile_scss_min', function(){
+gulp.task('compile_scss_min', function () {
 
 
   gulp.src(SCSS_SRC)
-  .pipe(sass().on('error', sass.logError))
-  .pipe(minifyCSS())
-  .pipe(rename({ suffix: '.min' }))
-  .pipe(changed(SCSS_DEST))
-  .pipe(gulp.dest(SCSS_DEST));
-
-
-  gulp.src(SCSS_SRC)
-  .pipe(sass().on('error', sass.logError))
-  .pipe(changed(SCSS_DEST))
-  .pipe(gulp.dest(SCSS_DEST));
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(minifyCSS())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(changed(SCSS_DEST))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(SCSS_DEST));
 
 });
 
 // Compile SCSS min
-gulp.task('compile_scss', function(){
+gulp.task('compile_scss', function () {
 
   gulp.src(SCSS_SRC)
-  .pipe(sass().on('error', sass.logError))
-  .pipe(changed(SCSS_DEST))
-  .pipe(gulp.dest(SCSS_DEST));
+    .pipe(sass().on('error', sass.logError))
+    .pipe(changed(SCSS_DEST))
+    .pipe(gulp.dest(SCSS_DEST));
 
 });
 
 
 // detect changes in SCSS
-gulp.task('watch_scss', function(){
+gulp.task('watch_scss', function () {
   gulp.watch(SCSS_SRC, ['compile_scss_min', 'compile_scss']);
 });
 
